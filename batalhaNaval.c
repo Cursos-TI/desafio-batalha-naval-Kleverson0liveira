13cdeb5f4f092e5926f2101b8e705f01cb57e424
Kleverson0liveira <oliveira.kleverson0@gmail.com>
2025-08-17 21:34:14 -0300

    adição de referencia cartografica

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -5,22 +5,34 @@
 int main() {
     
     // Inicialização do tabuleiro
-    int tabuleiro[10][10] = {0}; // Tabuleiro 10x10 inicializado com zeros      
+    int tabuleiro[10][11] = {0}; // Tabuleiro 10x10 inicializado com zeros     
+    
+    char letrasColuna[11] = {'#','A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'};
+    for(int i = 0; i<11; i++){
+        tabuleiro[i][0] = i;
+    }   
 
     // Exemplo de inicialização de navios
     for(int i =0; i <3; i++){
-        tabuleiro[i][0] = 1; // Colocando navios na primeira coluna
-        tabuleiro[3][i + 2] = 1; // Colocando navios na quarta linha
+        tabuleiro[i][7] = 1; // Colocando navios na primeira coluna
+        tabuleiro[3][i + 4] = 1; // Colocando navios na quarta linha
     }
 
+
+    
     // Exemplo de exibição do tabuleiro
-    printf("Tabuleiro Inicial:\n");
+    printf("Tabuleiro Inicial:\n \n");
+
+    for(int i=0; i<=10; i++){
+        printf("%c ", letrasColuna[i]);
+    }
+    printf("\n");
     for (int l = 0; l < 10; l++) {  
-        for (int c = 0; c < 10; c++) {
+        for (int c = 0; c < 11; c++) {
             printf("%d ", tabuleiro[l][c]);
         }
         printf("\n");
     }   
 
     return 0;
-}
+} 
